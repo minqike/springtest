2e7d0c15a2a43ce6411aca9a4687c5f0443ee4f5
minqike <minqike@foxmail.com>
2019-08-09 12:26:25 +0800

    增加数据库文件

---
+++ doc/db.sql
@@ -0,0 +1,64 @@
+/*
+SQLyog Ultimate v12.5.0 (64 bit)
+MySQL - 5.7.14-log : Database - 2019springtest
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`2019springtest` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `2019springtest`;
+
+/*Table structure for table `account` */
+
+DROP TABLE IF EXISTS `account`;
+
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `nickname` varchar(50) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `account` */
+
+insert  into `account`(`id`,`name`,`nickname`,`create_time`,`update_time`) values 
+(1,'zhang','张三','2019-08-08 20:43:39','2019-08-08 20:43:42'),
+(2,'li','李四','2019-08-08 20:43:56','2019-08-08 20:43:59'),
+(3,'wang','王五','2019-08-08 20:54:12','2019-08-08 20:54:12'),
+(4,'test','测试','2019-08-08 21:25:02','2019-08-08 21:25:02');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `avatar` varchar(200) DEFAULT NULL,
+  `created` date DEFAULT NULL,
+  `updated` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`username`,`password`,`avatar`,`created`,`updated`) values 
+(1,'zhang','111'','张三','2019-08-08','2019-08-08'),
+(2,'li','222','李四','2019-08-08','2019-08-08'),
+(3,'wang','333','王五','2019-08-08','2019-08-08');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
